aef75804cf85dc63d8df33570c381c3a99fca6a0
yaro <jarikexe@mail.ru>
2021-03-06 20:16:35 +0200

    i18n setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { InitialState } from './types/default';
 
 const App: React.FC = function () {
   const test = useSelector((state: InitialState) => state.test);
+  const { t } = useTranslation();
 
   return (
     <div className='App'>
@@ -12,6 +14,7 @@ const App: React.FC = function () {
         Hello World
       </Button>
       <div>{test}</div>
+      <div>{t('title')}</div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
+import './i18n';
 
 const store = createStore(rootReducer);
 
